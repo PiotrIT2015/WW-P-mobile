9203104a7691e6784f10f555b5fb4cf9c4d4bc28
PiotrIT2015 <piotr.prochnicki@gmail.com>
2025-01-04 18:10:06 +0100

    Feature-fix/add GlideModule

---
+++ app/src/main/java/engine/pp/ww_p_mobile/MyGlideApp.java
@@ -0,0 +1,7 @@
+package engine.pp.ww_p_mobile;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyGlideApp extends AppGlideModule {}
